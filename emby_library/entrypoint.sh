7b4fbcbcdc2574e2028ad9d3ff7a139c0e9107b5
DDSRem <1448139087@qq.com>
2024-01-12 22:51:55 +0800

    feat: crontab log

--- emby_library/entrypoint.sh
+++ emby_library/entrypoint.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+Green="\033[32m"
+Red="\033[31m"
+Yellow='\033[33m'
+Font="\033[0m"
+INFO="[${Green}INFO${Font}]"
+ERROR="[${Red}ERROR${Font}]"
+WARN="[${Yellow}WARN${Font}]"
+function INFO() {
+    echo -e "${INFO} ${1}"
+}
+function ERROR() {
+    echo -e "${ERROR} ${1}"
+}
+function WARN() {
+    echo -e "${WARN} ${1}"
+}
+
 bash /app/update.sh update_config
 bash /app/update.sh update_policy
 
 crontab -r
 echo -e "${CRON} bash /app/update.sh update_config && bash /app/update.sh update_policy" >> /tmp/crontab.list
-echo "Set crontab to system..."
+INFO "设置定时任务中..."
 crontab /tmp/crontab.list
-echo "Current crontab is:"
+INFO "定时任务预览:"
 crontab -l
 rm -f /tmp/crontab.list
 
