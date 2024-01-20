0cefdc2257b46c40098c19c3dee6ac993266bc43
DDSRem <1448139087@qq.com>
2024-01-20 14:25:31 +0800

    Revert "feat: cache xiaoya_alist logo"
    This reverts commit df1e5d9e47ee45cc7db2e1ee4c0a92bcfaa2d90d.

--- main.sh
+++ main.sh
@@ -1481,10 +1481,7 @@ function main_advanced_configuration(){
 }
 
 function main_return(){
-    if [ ! -f /tmp/xiaoya_alist ]; then
-        curl -sL https://ddsrem.com/xiaoya/xiaoya_alist -o /tmp/xiaoya_alist
-    fi
-    cat /tmp/xiaoya_alist
+    curl -sL https://ddsrem.com/xiaoya/xiaoya_alist
     echo -e "1、安装/更新/卸载 小雅Alist"
     echo -e "2、安装/卸载 小雅Emby全家桶"
     echo -e "3、安装/更新/卸载 小雅助手（xiaoyahelper）"
@@ -1559,5 +1556,3 @@ if [ ! $@ ]; then
 else
     $@
 fi
-
-rm -rf /tmp/xiaoya_alist
