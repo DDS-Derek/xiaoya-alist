d162c9f5d7b3d98efaa9f161e78dabf5c3b6e999
DDSRem <1448139087@qq.com>
2024-01-26 20:19:12 +0800

    feat: installation status

--- main.sh
+++ main.sh
@@ -1541,14 +1541,24 @@ function main_advanced_configuration(){
 
 }
 
+function judgment_container(){
+
+    if docker container inspect ${1} >/dev/null 2>&1; then
+        echo -e "${Green}已安装${Font}"
+    else
+        echo -e "${Red}未安装${Font}"
+    fi
+
+}
+
 function main_return(){
     curl -sL https://ddsrem.com/xiaoya/xiaoya_alist | sed "s/DATE_VERSION/${DATE_VERSION}/g"
-    echo -e "1、安装/更新/卸载 小雅Alist"
-    echo -e "2、安装/卸载 小雅Emby全家桶"
+    echo -e "1、安装/更新/卸载 小雅Alist   当前状态：$(judgment_container $(xiaoya_alist_name))"
+    echo -e "2、安装/卸载 小雅Emby全家桶   当前状态：$(judgment_container $(xiaoya_emby_name))"
     echo -e "3、安装/更新/卸载 小雅助手（xiaoyahelper）"
-    echo -e "4、安装/更新/卸载 小雅Alist-TVBox"
-    echo -e "5、安装/更新/卸载 Onelist"
-    echo -e "6、安装/更新/卸载 Portainer"
+    echo -e "4、安装/更新/卸载 小雅Alist-TVBox   当前状态：$(judgment_container $(xiaoya_tvbox_name))"
+    echo -e "5、安装/更新/卸载 Onelist   当前状态：$(judgment_container $(xiaoya_onelist_name))"
+    echo -e "6、安装/更新/卸载 Portainer   当前状态：$(judgment_container $(portainer_name))"
     echo -e "7、高级配置"
     echo -e "8、退出脚本"
     echo -e "——————————————————————————————————————————————————————————————————————————————————"
@@ -1640,7 +1650,7 @@ function first_init(){
 
 }
 
-if [ ! $@ ]; then
+if [ ! "$@" ]; then
     first_init
     main
 elif [ "$@" == test ]; then
