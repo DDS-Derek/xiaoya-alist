e1aa15e96f5a60ea742153f5c2fbea01f29d5858
DDSRem <1448139087@qq.com>
2024-03-09 17:47:48 +0800

    feat: integrate notify script

--- xiaoya_notify.sh
+++ xiaoya_notify.sh
@@ -11,14 +11,29 @@ Font="\033[0m"
 INFO="[${Green}INFO${Font}]"
 ERROR="[${Red}ERROR${Font}]"
 WARN="[${Yellow}WARN${Font}]"
-function INFO() {
-    echo -e "${INFO} ${1}"
+Time=$(date +"%Y-%m-%d %T")
+function INFO(){
+    echo -e "${Time} ${INFO} ${1}"
 }
-function ERROR() {
-    echo -e "${ERROR} ${1}"
+function ERROR(){
+    echo -e "${Time} ${ERROR} ${1}"
 }
-function WARN() {
-    echo -e "${WARN} ${1}"
+function WARN(){
+    echo -e "${Time} ${WARN} ${1}"
+}
+
+function container_update() {
+
+    docker pull containrrr/watchtower:latest
+    docker run --rm \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        containrrr/watchtower:latest \
+        --run-once \
+        --cleanup \
+        "${@}"
+    docker rmi containrrr/watchtower:latest
+    INFO "${*} 更新成功"
+
 }
 
 function pull_run_glue() {
@@ -90,6 +105,7 @@ function test_xiaoya_status() {
                 xiaoya_addr=${docker_address}
             else
                 ERROR "请检查xiaoya是否正常运行后再试"
+                docker logs --tail 8 ${XIAOYA_NAME}
                 exit 1
             fi
         else
@@ -99,29 +115,6 @@ function test_xiaoya_status() {
     fi
 
     INFO "连接小雅地址为 ${xiaoya_addr}"
-
-}
-
-function compare_metadata_size() {
-
-    pull_run_glue xh --headers --follow -o /media/headers.log "${xiaoya_addr}/d/元数据/${1}"
-    REMOTE_METADATA_SIZE=$(cat ${MEDIA_DIR}/headers.log | grep 'Content-Length' | awk '{print $2}')
-    rm -f ${MEDIA_DIR}/headers.log
-
-    if [ -f "${MEDIA_DIR}/temp/${1}" ] && [ ! -f "${MEDIA_DIR}/temp/${1}.aria2" ]; then
-        LOCAL_METADATA_SIZE=$(du -b "${MEDIA_DIR}/temp/${1}" | awk '{print $1}')
-    else
-        LOCAL_METADATA_SIZE=0
-    fi
-
-    INFO "${1} REMOTE_METADATA_SIZE: ${REMOTE_METADATA_SIZE}"
-    INFO "${1} LOCAL_METADATA_SIZE: ${LOCAL_METADATA_SIZE}"
-
-    if [ "${REMOTE_METADATA_SIZE}" == "${LOCAL_METADATA_SIZE}" ]; then
-        __COMPARE_METADATA_SIZE=1
-    else
-        __COMPARE_METADATA_SIZE=2
-    fi
 
 }
 
@@ -204,6 +197,107 @@ function update_media() {
 
 }
 
+function compare_metadata_size() {
+
+    pull_run_glue xh --headers --follow -o /media/headers.log "${xiaoya_addr}/d/元数据/${1}"
+    REMOTE_METADATA_SIZE=$(cat ${MEDIA_DIR}/headers.log | grep 'Content-Length' | awk '{print $2}')
+    rm -f ${MEDIA_DIR}/headers.log
+
+    if [ -f "${MEDIA_DIR}/temp/${1}" ] && [ ! -f "${MEDIA_DIR}/temp/${1}.aria2" ]; then
+        LOCAL_METADATA_SIZE=$(du -b "${MEDIA_DIR}/temp/${1}" | awk '{print $1}')
+    else
+        LOCAL_METADATA_SIZE=0
+    fi
+
+    INFO "${1} REMOTE_METADATA_SIZE: ${REMOTE_METADATA_SIZE}"
+    INFO "${1} LOCAL_METADATA_SIZE: ${LOCAL_METADATA_SIZE}"
+
+    if [ "${REMOTE_METADATA_SIZE}" == "${LOCAL_METADATA_SIZE}" ]; then
+        __COMPARE_METADATA_SIZE=1
+    else
+        __COMPARE_METADATA_SIZE=2
+    fi
+
+}
+
+function detection_all_pikpak_update() {
+
+    compare_metadata_size "all.mp4"
+    if [ "${__COMPARE_METADATA_SIZE}" == "1" ]; then
+        INFO "跳过 all.mp4 更新"
+    else
+        update_media "all.mp4"
+    fi
+
+    compare_metadata_size "pikpak.mp4"
+    if [ "${__COMPARE_METADATA_SIZE}" == "1" ]; then
+        INFO "跳过 pikpak.mp4 更新"
+    else
+        update_media "pikpak.mp4"
+    fi
+
+    INFO "全部媒体元数据更新完成！"
+
+}
+
+function detection_config_update() {
+
+    compare_metadata_size "config.mp4"
+    if [ "${__COMPARE_METADATA_SIZE}" == "1" ]; then
+        INFO "跳过 config.mp4 更新"
+    else
+        bash -c "$(curl http://docker.xiaoya.pro/sync_emby_config.sh)" -s ${MEDIA_DIR} ${CONFIG_DIR} ${EMBY_NAME} ${RESILIO_NAME}
+    fi
+
+}
+
+function detection_xiaoya_version_update() {
+
+    REMOTE_XIAOYA_VERSION=$(curl -skL https://docker.xiaoya.pro/version.txt | head -n 1 | sed "s/\r$//g")
+
+    LOCAL_XIAOYA_VERSION=$(docker exec -it ${XIAOYA_NAME} cat /version.txt | head -n 1 | sed "s/\r$//g")
+
+    INFO "REMOTE_XIAOYA_VERSION: ${REMOTE_XIAOYA_VERSION}"
+    INFO "LOCAL_XIAOYA_VERSION: ${LOCAL_XIAOYA_VERSION}"
+
+    if [ "${REMOTE_XIAOYA_VERSION}" == "${LOCAL_XIAOYA_VERSION}" ] || [ "${REMOTE_XIAOYA_VERSION}" == "" ]; then
+        INFO "跳过小雅容器重启"
+    else
+        docker restart ${XIAOYA_NAME}
+    fi
+
+}
+
+function detection_xiaoya_image_update() {
+
+    if docker inspect xiaoyaliu/alist:latest > /dev/null 2>&1; then
+        if docker inspect xiaoyaliu/alist:latest > /dev/null 2>&1; then
+            local_sha=$(docker inspect --format='{{index .RepoDigests 0}}' xiaoyaliu/alist:latest | cut -f2 -d:)
+            remote_sha=$(curl -s "https://hub.docker.com/v2/repositories/xiaoyaliu/alist/tags/latest" | grep -o '"digest":"[^"]*' | grep -o '[^"]*$' | tail -n1 | cut -f2 -d:)
+            INFO "remote_sha: ${remote_sha}"
+            INFO "local_sha: ${local_sha}"
+            if [ ! "${local_sha}" == "${remote_sha}" ]; then
+                container_update "${XIAOYA_NAME}"
+            else
+                INFO "跳过小雅容器更新"
+            fi
+        fi
+    elif docker inspect xiaoyaliu/alist:hostmode > /dev/null 2>&1; then
+        if docker inspect xiaoyaliu/alist:hostmode > /dev/null 2>&1; then
+            local_sha=$(docker inspect --format='{{index .RepoDigests 0}}' xiaoyaliu/alist:hostmode | cut -f2 -d:)
+            remote_sha=$(curl -s "https://hub.docker.com/v2/repositories/xiaoyaliu/alist/tags/hostmode" | grep -o '"digest":"[^"]*' | grep -o '[^"]*$' | tail -n1 | cut -f2 -d:)
+            INFO "remote_sha: ${remote_sha}"
+            INFO "local_sha: ${local_sha}"
+            if [ ! "${local_sha}" == "${remote_sha}" ]; then
+                container_update "${XIAOYA_NAME}"
+            else
+                INFO "跳过小雅容器更新"
+            fi
+        fi
+    fi
+
+}
+
 if [ ! "$1" ]; then
     ERROR "请配置媒体目录后重试！"
 else
@@ -228,25 +322,24 @@ else
     RESILIO_NAME=resilio
 fi
 
+if [ "$5" ]; then
+    XIAOYA_NAME=$5
+else
+    XIAOYA_NAME=xiaoya
+fi
+
 INFO "小雅配置目录：${CONFIG_DIR}"
 INFO "媒体库目录：${MEDIA_DIR}"
 INFO "Emby 容器名称：${EMBY_NAME}"
 INFO "Resilio 容器名称：${RESILIO_NAME}"
+INFO "小雅容器名称：${XIAOYA_NAME}"
 
 test_xiaoya_status
-
-compare_metadata_size "all.mp4"
-if [ "${__COMPARE_METADATA_SIZE}" == "1" ]; then
-    INFO "跳过 all.mp4 更新"
-else
-    update_media "all.mp4"
-fi
-
-compare_metadata_size "pikpak.mp4"
-if [ "${__COMPARE_METADATA_SIZE}" == "1" ]; then
-    INFO "跳过 pikpak.mp4 更新"
-else
-    update_media "pikpak.mp4"
-fi
-
-INFO "全部媒体元数据更新完成！"
+# all.mp4 和 pikpak.mp4
+detection_all_pikpak_update
+# config.mp4
+detection_config_update
+# xiaoya version
+detection_xiaoya_version_update
+# xiaoya image
+detection_xiaoya_image_update
