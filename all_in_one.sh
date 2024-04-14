7a6f463e76bcaf1433d5bdd805840187f2f554ca
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-04-14 16:25:34 +0800

    feat: optimize interface display

--- all_in_one.sh
+++ all_in_one.sh
@@ -4289,7 +4289,7 @@ function main_return() {
     echo -e "5、安装/更新/卸载 小雅Alist-TVBox             当前安装状态：$(judgment_container "${xiaoya_tvbox_name}")"
     echo -e "6、安装/更新/卸载 Onelist                     当前安装状态：$(judgment_container "${xiaoya_onelist_name}")"
     echo -e "7、其他工具 | Script info: ${DATE_VERSION} OS: ${_os},${OSNAME},${is64bit}"
-    echo -e "8、高级配置 | Docker version: ${DOCKER_VERSION} City: ${CITY}"
+    echo -e "8、高级配置 | Docker version: ${Sky_Blue}${DOCKER_VERSION}${Font} IP City: ${Sky_Blue}${CITY}${Font}"
     echo -e "0、退出脚本 | Thanks: ${Sky_Blue}heiheigui,xiaoyaLiu,Harold,AI老G${Font}"
     echo -e "——————————————————————————————————————————————————————————————————————————————————"
     read -erp "请输入数字 [0-8]:" num
