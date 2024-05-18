dcb657031d4a19f816dc94879d031fef59022660
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-05-18 11:22:15 +0800

    fix: sh-checker supported errors

--- emby_img/run.sh
+++ emby_img/run.sh
@@ -3,20 +3,20 @@
 # shellcheck disable=SC2114
 
 if [ -f /media.img ]; then
-	if [ ! -d /volume_img ]; then
-		mkdir /volume_img
-	fi
-	if grep -qs '/volume_img' /proc/mounts; then
-		umount /volume_img
-		wait ${!}
-	fi
-	mount -o loop /media.img /volume_img
-	echo "img 镜像挂载成功！"
-	if [ -d /media ]; then
-		rm -rf /media
-	fi
-	ln -sf /volume_img/xiaoya /media
-	echo "媒体库软连接创建成功！"
+    if [ ! -d /volume_img ]; then
+        mkdir /volume_img
+    fi
+    if grep -qs '/volume_img' /proc/mounts; then
+        umount /volume_img
+        wait ${!}
+    fi
+    mount -o loop /media.img /volume_img
+    echo "img 镜像挂载成功！"
+    if [ -d /media ]; then
+        rm -rf /media
+    fi
+    ln -sf /volume_img/xiaoya /media
+    echo "媒体库软连接创建成功！"
 else
-	echo "img 镜像未挂载，跳过自动挂载！"
+    echo "img 镜像未挂载，跳过自动挂载！"
 fi
