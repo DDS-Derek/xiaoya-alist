cfb1249944b6aba4e4ef9eca3c9d0f90ab5b666a
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-05-25 17:23:12 +0800

    fix: https://github.com/DDS-Derek/xiaoya-alist/commit/6ce8614cf04983a133e8a59dddf61408b8c8602a

--- emby_metadata_downloader/entrypoint.sh
+++ emby_metadata_downloader/entrypoint.sh
@@ -45,7 +45,7 @@ function mount_img() {
         sleep 30
     done
     if [ -d /media ]; then
-        if [ -d /media/电影/2023 ]; then
+        if [ ! -d /media/电影/2023 ]; then
             if ! rm -rf /media; then
                 ERROR '删除 /media 失败！使用老G速装版emby请勿将任何目录挂载到容器的 /media 目录！程序退出！'
                 exit 1
