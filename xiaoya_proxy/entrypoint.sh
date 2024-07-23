7aea35dfb18062d6a83d312948f3e963d402d852
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-07-23 18:44:03 +0800

    fix: xiaoya proxy start error

--- xiaoya_proxy/entrypoint.sh
+++ xiaoya_proxy/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec dumb-init java -jar -Dfile.encoding=UTF-8 -Xmx64m /xiaoya_proxy.jar
+exec dumb-init java -jar -Dfile.encoding=UTF-8 -Xmx64m /xiaoya_proxy.jar com.ddsrem.xiaoya_proxy.XiaoyaProxyRun
