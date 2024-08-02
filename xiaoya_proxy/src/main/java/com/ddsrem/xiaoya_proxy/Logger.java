c38b821e5c65a1ba71fdc4cf34164ca31ca3d976
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-08-02 21:27:39 +0800

    feat: xiaoya proxy log output command line

--- xiaoya_proxy/src/main/java/com/ddsrem/xiaoya_proxy/Logger.java
+++ xiaoya_proxy/src/main/java/com/ddsrem/xiaoya_proxy/Logger.java
@@ -1,23 +1,13 @@
 package com.ddsrem.xiaoya_proxy;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Logger {
-    static boolean dbg = false;
+    static boolean dbg = true;
+    
     public static void log(String message, boolean force) {
         if(!dbg && !force){
             return;
         }
-        String filePath = "/storage/emulated/0/TV/log.txt";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
-            writer.write(message);
-            writer.newLine();
-            writer.newLine();
-        } catch (IOException e) {
-            System.err.println("Error writing to log file: " + e.getMessage());
-        }
+        System.out.println(message);
     }
     
     public static void log(String message) {
