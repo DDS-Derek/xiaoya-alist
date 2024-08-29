29617f94a6b7b12bf19b479369102938941c320c
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-08-29 10:21:09 +0800

    feat: code modularity

--- all_in_one.sh
+++ all_in_one.sh
@@ -46,6 +46,7 @@ function WARN() {
     echo -e "${WARN} ${1}"
 }
 
+# shellcheck disable=SC2034
 mirrors=(
     "docker.io"
     "registry-docker-hub-latest-9vqc.onrender.com"
@@ -3792,116 +3793,6 @@ function main_xiaoya_115_cleaner() {
 
 }
 
-function install_onelist() {
-
-    if [ -f ${DDSREM_CONFIG_DIR}/onelist_config_dir.txt ]; then
-        OLD_CONFIG_DIR=$(cat ${DDSREM_CONFIG_DIR}/onelist_config_dir.txt)
-        INFO "已读取Onelist配置文件路径：${OLD_CONFIG_DIR} (默认不更改回车继续，如果需要更改请输入新路径)"
-        read -erp "CONFIG_DIR:" CONFIG_DIR
-        [[ -z "${CONFIG_DIR}" ]] && CONFIG_DIR=${OLD_CONFIG_DIR}
-        echo "${CONFIG_DIR}" > ${DDSREM_CONFIG_DIR}/onelist_config_dir.txt
-    else
-        INFO "请输入配置文件目录（默认 /etc/onelist ）"
-        read -erp "CONFIG_DIR:" CONFIG_DIR
-        [[ -z "${CONFIG_DIR}" ]] && CONFIG_DIR="/etc/onelist"
-        touch ${DDSREM_CONFIG_DIR}/onelist_config_dir.txt
-        echo "${CONFIG_DIR}" > ${DDSREM_CONFIG_DIR}/onelist_config_dir.txt
-    fi
-
-    INFO "请输入后台管理端口（默认 5245 ）"
-    read -erp "HT_PORT:" HT_PORT
-    [[ -z "${HT_PORT}" ]] && HT_PORT="5245"
-
-    docker_pull "msterzhang/onelist:latest"
-
-    docker run -itd \
-        -p "${HT_PORT}":5245 \
-        -e PUID=0 \
-        -e PGID=0 \
-        -e UMASK=022 \
-        -e TZ=Asia/Shanghai \
-        -v "${CONFIG_DIR}:/config" \
-        --restart=always \
-        --name="$(cat ${DDSREM_CONFIG_DIR}/container_name/xiaoya_onelist_name.txt)" \
-        msterzhang/onelist:latest
-
-    INFO "安装完成！"
-
-}
-
-function update_onelist() {
-
-    for i in $(seq -w 3 -1 0); do
-        echo -en "即将开始更新Onelist${Blue} $i ${Font}\r"
-        sleep 1
-    done
-    container_update "$(cat ${DDSREM_CONFIG_DIR}/container_name/xiaoya_onelist_name.txt)"
-
-}
-
-function uninstall_onelist() {
-
-    INFO "是否${Red}删除配置文件${Font} [Y/n]（默认 Y 删除）"
-    read -erp "Clean config:" CLEAN_CONFIG
-    [[ -z "${CLEAN_CONFIG}" ]] && CLEAN_CONFIG="y"
-
-    for i in $(seq -w 3 -1 0); do
-        echo -en "即将开始卸载 Onelist${Blue} $i ${Font}\r"
-        sleep 1
-    done
-    docker stop "$(cat ${DDSREM_CONFIG_DIR}/container_name/xiaoya_onelist_name.txt)"
-    docker rm "$(cat ${DDSREM_CONFIG_DIR}/container_name/xiaoya_onelist_name.txt)"
-    docker rmi msterzhang/onelist:latest
-    if [[ ${CLEAN_CONFIG} == [Yy] ]]; then
-        INFO "清理配置文件..."
-        if [ -f ${DDSREM_CONFIG_DIR}/onelist_config_dir.txt ]; then
-            OLD_CONFIG_DIR=$(cat ${DDSREM_CONFIG_DIR}/onelist_config_dir.txt)
-            rm -rf "${OLD_CONFIG_DIR}"
-        fi
-    fi
-    INFO "Onelist 卸载成功！"
-
-}
-
-function main_onelist() {
-
-    echo -e "——————————————————————————————————————————————————————————————————————————————————"
-    echo -e "${Blue}Onelist${Font}\n"
-    echo -e "1、安装"
-    echo -e "2、更新"
-    echo -e "3、卸载"
-    echo -e "0、返回上级"
-    echo -e "——————————————————————————————————————————————————————————————————————————————————"
-    read -erp "请输入数字 [0-3]:" num
-    case "$num" in
-    1)
-        clear
-        install_onelist
-        return_menu "main_onelist"
-        ;;
-    2)
-        clear
-        update_onelist
-        return_menu "main_onelist"
-        ;;
-    3)
-        clear
-        uninstall_onelist
-        return_menu "main_onelist"
-        ;;
-    0)
-        clear
-        main_other_tools
-        ;;
-    *)
-        clear
-        ERROR '请输入正确数字 [0-3]'
-        main_onelist
-        ;;
-    esac
-
-}
-
 function install_xiaoya_proxy() {
 
     local config_dir
@@ -4031,39 +3922,6 @@ function main_xiaoya_proxy() {
 
 }
 
-function main_casaos() {
-
-    echo -e "——————————————————————————————————————————————————————————————————————————————————"
-    echo -e "${Blue}CasaOS${Font}\n"
-    echo -e "1、安装"
-    echo -e "2、卸载"
-    echo -e "0、返回上级"
-    echo -e "——————————————————————————————————————————————————————————————————————————————————"
-    read -erp "请输入数字 [0-2]:" num
-    case "$num" in
-    1)
-        clear
-        curl -fsSL https://get.casaos.io | sudo bash
-        return_menu "main_casaos"
-        ;;
-    2)
-        clear
-        casaos-uninstall
-        return_menu "main_casaos"
-        ;;
-    0)
-        clear
-        main_other_tools
-        ;;
-    *)
-        clear
-        ERROR '请输入正确数字 [0-2]'
-        main_casaos
-        ;;
-    esac
-
-}
-
 function main_docker_compose() {
 
     echo -e "——————————————————————————————————————————————————————————————————————————————————"
@@ -4108,155 +3966,6 @@ function main_docker_compose() {
         main_docker_compose
         ;;
     esac
-
-}
-
-function auto_choose_image_mirror() {
-
-    for i in "${!mirrors[@]}"; do
-        local output
-        output=$(
-            curl -s -o /dev/null -m 4 -w '%{time_total}' --head --request GET "${mirrors[$i]}"
-            echo $? > /tmp/curl_exit_status_${i} &
-        )
-        # shellcheck disable=SC2004
-        status[$i]=$!
-        # shellcheck disable=SC2004
-        delays[$i]=$output
-    done
-    better_time=9999999999
-    for i in "${!mirrors[@]}"; do
-        local time_compare result
-        wait ${status[$i]}
-        result=$(cat /tmp/curl_exit_status_${i})
-        rm -f /tmp/curl_exit_status_${i}
-        if [ $result -eq 0 ]; then
-            if [ "${mirrors[$i]}" == "docker.io" ]; then
-                time_compare=$(awk -v n1="1" -v n2="$result" 'BEGIN {print (n1>n2)? "1":"0"}')
-                if [ $time_compare -eq 1 ]; then
-                    better_mirror=${mirrors[$i]}
-                    better_time=0
-                fi
-            else
-                time_compare=$(awk -v n1="$better_time" -v n2="$result" 'BEGIN {print (n1>n2)? "1":"0"}')
-                if [ $time_compare -eq 1 ]; then
-                    better_mirror=${mirrors[$i]}
-                    better_time=${delays[$i]}
-                fi
-            fi
-        fi
-    done
-    if [ -z "${better_mirror}" ]; then
-        return 1
-    else
-        echo -e "${better_mirror}" > ${DDSREM_CONFIG_DIR}/image_mirror.txt
-        if docker pull "${better_mirror}/library/hello-world:latest" &> /dev/null; then
-            docker rmi "${better_mirror}/library/hello-world:latest" &> /dev/null
-            return 0
-        else
-            return 1
-        fi
-    fi
-
-}
-
-function choose_image_mirror() {
-
-    local num
-    local current_mirror interface
-    current_mirror="$(cat "${DDSREM_CONFIG_DIR}/image_mirror.txt")"
-    declare -i s
-    local s=0
-    echo -e "——————————————————————————————————————————————————————————————————————————————————"
-    echo -e "${Blue}Docker镜像源选择\n${Font}"
-    echo -ne "${INFO} 界面加载中...${Font}\r"
-    interface="${Sky_Blue}绿色字体代表当前选中的镜像源"
-    interface="${interface}\n选择镜像源后会自动检测是否可连接，如果预选镜像源都无法使用请自定义镜像源${Font}\n"
-    local status=()
-    for i in "${!mirrors[@]}"; do
-        local output
-        output=$(
-            curl -s -o /dev/null -m 4 -w '%{time_total}' --head --request GET "${mirrors[$i]}"
-            echo $? > /tmp/curl_exit_status_${i} &
-        )
-        # shellcheck disable=SC2004
-        status[$i]=$!
-        # shellcheck disable=SC2004
-        delays[$i]=$(printf "%.2f" $output)
-    done
-    for i in "${!mirrors[@]}"; do
-        wait ${status[$i]}
-        local result
-        result=$(cat /tmp/curl_exit_status_${i})
-        rm -f /tmp/curl_exit_status_${i}
-        local color=
-        local font=
-        if [[ "${mirrors[$i]}" == "${current_mirror}" ]]; then
-            color="${Green}"
-            font="${Font}"
-            s+=1
-        fi
-        if [ $result -eq 0 ]; then
-            interface="${interface}\n$((i + 1))、${color}${mirrors[$i]}${font} (${Green}可用${Font} ${Sky_Blue}延迟: ${delays[$i]}秒${Font})"
-        else
-            interface="${interface}\n$((i + 1))、${color}${mirrors[$i]}${font} (${Red}不可用${Font})"
-        fi
-        z=$((i + 2))
-    done
-    if user_delay=$(curl -s -o /dev/null -m 4 -w '%{time_total}' --head --request GET "$(cat "${DDSREM_CONFIG_DIR}/image_mirror_user.txt")"); then
-        USER_TEST_STATUS="(${Green}可用${Font} ${Sky_Blue}延迟: ${user_delay}秒${Font})"
-    else
-        USER_TEST_STATUS="(${Red}不可用${Font})"
-    fi
-    if [ "${s}" == "1" ]; then
-        interface="${interface}\n${z}、自定义源：$(cat "${DDSREM_CONFIG_DIR}/image_mirror_user.txt") ${USER_TEST_STATUS}"
-    else
-        interface="${interface}\n${z}、${Green}自定义源：$(cat "${DDSREM_CONFIG_DIR}/image_mirror_user.txt")${Font} ${USER_TEST_STATUS}"
-    fi
-    echo -e "${interface}\n0、返回上级"
-    echo -e "——————————————————————————————————————————————————————————————————————————————————"
-    read -erp "请输入数字 [0-${z}]:" num
-    if [ "${num}" == "0" ]; then
-        clear
-        "${1}"
-    elif [ "${num}" == "${z}" ]; then
-        clear
-        INFO "请输入自定义源地址（当前自定义源地址为：$(cat "${DDSREM_CONFIG_DIR}/image_mirror_user.txt")，回车默认不修改）"
-        read -erp "custom_url:" custom_url
-        [[ -z "${custom_url}" ]] && custom_url=$(cat "${DDSREM_CONFIG_DIR}/image_mirror_user.txt")
-        echo "${custom_url}" > ${DDSREM_CONFIG_DIR}/image_mirror.txt
-        echo "${custom_url}" > ${DDSREM_CONFIG_DIR}/image_mirror_user.txt
-    else
-        for i in "${!mirrors[@]}"; do
-            if [[ "$((i + 1))" == "${num}" ]]; then
-                echo -e "${mirrors[$i]}" > ${DDSREM_CONFIG_DIR}/image_mirror.txt
-                break
-            fi
-        done
-    fi
-    clear
-    INFO "开始镜像源地址连通性测试..."
-    local retries=0
-    local max_retries=3
-    IMAGE_MIRROR=$(cat "${DDSREM_CONFIG_DIR}/image_mirror.txt")
-    while [ $retries -lt $max_retries ]; do
-        if docker pull "${IMAGE_MIRROR}/library/hello-world:latest"; then
-            INFO "地址连通性测试正常！"
-            break
-        else
-            WARN "地址连通性测试失败，正在进行第 $((retries + 1)) 次重试..."
-            retries=$((retries + 1))
-        fi
-    done
-    if [ $retries -eq $max_retries ]; then
-        ERROR "地址连通性测试失败，已达到最大重试次数，请选择镜像源或者自定义镜像源！"
-    else
-        docker rmi "${IMAGE_MIRROR}/library/hello-world:latest"
-    fi
-    INFO "按任意键返回 Docker镜像源选择 菜单"
-    read -rs -n 1 -p ""
-    clear
-    choose_image_mirror "${1}"
 
 }
 
@@ -4779,7 +4488,7 @@ root_need
 if [ ! -d "/tmp/xiaoya_alist_tmp" ]; then
     mkdir -p /tmp/xiaoya_alist_tmp
 fi
-for file in "auto_symlink" "jellyfin" "portainer" "base"; do
+for file in "base" "image_mirror" "auto_symlink" "jellyfin" "portainer" "onelist" "casaos"; do
     if ! curl -sSLf "https://gitee.com/ddsrem/xiaoya-alist-base/raw/master/${file}.sh" -o "/tmp/xiaoya_alist_tmp/${file}.sh"; then
         ERROR "${file} 基础库获取失败！"
         ERROR "请检查是否能访问 gitee.com！"
