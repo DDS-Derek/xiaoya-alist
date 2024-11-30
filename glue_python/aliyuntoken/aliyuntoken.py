42bb8c9bb49699f7dec2979dde649fce54c53851
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-11-30 11:12:36 +0800

    feat: aliyunpan refresh token support shell qrcode

--- glue_python/aliyuntoken/aliyuntoken.py
+++ glue_python/aliyuntoken/aliyuntoken.py
@@ -8,7 +8,10 @@ import logging
 import os
 import threading
 import sys
+import qrcode
+import argparse
 from PIL import Image
+import pyzbar.pyzbar as pyzbar
 from io import BytesIO
 from flask import Flask, send_file, render_template, jsonify
 
@@ -39,7 +42,7 @@ headers = {
 }
 
 
-def poll_qrcode_status(data):
+def poll_qrcode_status(data, log_print):
     global last_status
     while True:
         re = requests.post('https://api.xhofe.top/alist/ali/ck', json=data, headers=headers)
@@ -59,8 +62,16 @@ def poll_qrcode_status(data):
                 last_status = 1
                 break
             else:
-                logging.info('等待用户扫码...')
+                if log_print:
+                    logging.info('等待用户扫码...')
                 time.sleep(2)
+
+
+def decode_qr_code(qr_code_image_path):
+    img = Image.open(qr_code_image_path)
+    decoded_objects = pyzbar.decode(img)
+    for obj in decoded_objects:
+        return obj.data.decode('utf-8')
 
 
 @app.route("/")
@@ -93,7 +104,11 @@ def shutdown():
 if __name__ == '__main__':
     if os.path.isfile(qrcode_dir):
         os.remove(qrcode_dir)
+    parser = argparse.ArgumentParser(description='AliyunPan Refresh Token')
+    parser.add_argument('--qrcode_mode', type=str, help='扫码模式')
+    args = parser.parse_args()
     logging.info('二维码生成中...')
+    re_count = 0
     while True:
         re = requests.get('https://api.xhofe.top/alist/ali/qr', headers=headers)
         if re.status_code == 200:
@@ -111,7 +126,27 @@ if __name__ == '__main__':
                 if os.path.isfile(qrcode_dir):
                     logging.info('二维码生成完成！')
                     break
-                else:
-                    time.sleep(1)
-    threading.Thread(target=poll_qrcode_status, args=(data,)).start()
-    app.run(host='0.0.0.0', port=34256)
+        time.sleep(1)
+        re_count += 1
+        if re_count == 3:
+            logging.error('二维码生成失败，退出进程')
+            os._exit(1)
+    if args.qrcode_mode == 'web':
+        threading.Thread(target=poll_qrcode_status, args=(data, True)).start()
+        app.run(host='0.0.0.0', port=34256)
+    elif args.qrcode_mode == 'shell':
+        threading.Thread(target=poll_qrcode_status, args=(data, False)).start()
+        decoded_data = decode_qr_code(qrcode_dir)
+        qr = qrcode.QRCode(border=1)
+        qr.add_data(decoded_data)
+        qr.make(fit=True)
+        qr.make_image(fill_color="black", back_color="white")
+        qr.print_ascii(invert=True, tty=sys.stdout.isatty())
+        while last_status != 1:
+            time.sleep(1)
+        if os.path.isfile(qrcode_dir):
+            os.remove(qrcode_dir)
+        os._exit(0)
+    else:
+        logging.error('未知的扫码模式')
+        os._exit(1)
